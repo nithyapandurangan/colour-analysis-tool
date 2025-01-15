c24624583831c4887f20be083464320b7b3cd480
Nithya P <111881865+nithyapandurangan@users.noreply.github.com>
2025-01-15 22:41:27 +0530

    Update Intro.jsx

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import Colourwheel from "../../assets/images/colourwheel.jpeg"
 import line from "../../assets/images/Vector-light.png"
 import linemain from "../../assets/images/Vector.png"
-import bulb1 from "../../assets/images/remove-bg-1-2.png";
-import bulb2 from "../../assets/images/remove-bg-1.png";
 
 const Intro = (props) => {
   return (
     <div className="w-full flex min-h-screen items-center flex-col bg-white">
         <div className="relative w-full flex flex-col items-end p-16">
-        {/* <img className="absolute w-[91px] h-[115px] top-[100px] left-[185px]" alt="Remove bg" src={bulb1} />
-        <img className="absolute w-[82px] h-[114px] top-[100px] left-[1500px]" alt="Remove bg" src={bulb2} /> */}
 
           {/* Title */}
           <div className="absolute top-10 left-10 flex flex-col items-center w-full">
@@ -22,50 +18,50 @@ const Intro = (props) => {
 
 
         {/* Intro */}
-          <span className="absolute top-[180px] left-[500px] text-lg text-center w-[900px] text-black">
+          <span className="absolute top-[180px] left-[20%] text-lg text-center w-[900px] text-black">
             Our Colour analysis tool determines which colours flatter you most based on your eye, hair, and skin colour. You will then be assigned one of 4 seasons- Spring, Summer, Autumn & Winter. Each colour season comes with a colour palette, specifically designed to harmonize with your features.
           </span>
           </div>
 
           {/* Spring Section */}
-          <div className="absolute top-[350px] left-[350px] flex items-start">
+          <div className="absolute top-[350px] left-[200px] flex items-start">
             <img src={line} alt="Vector" className="absolute -bottom-3 left-1" />
             <span className="text-4xl font-bold text-black">Spring</span>
           </div>
-          <span className="absolute top-[450px] left-[150px] text-lg text-black w-[500px] text-center">
+          <span className="absolute top-[450px] left-[50px] text-lg text-black w-[500px] text-center">
             People who fall under the Spring category typically have warm undertones with light, clear features. Their overall colouring is warm, fresh, and often delicate, meaning they look best in bright, warm colours that bring out their natural radiance.
           </span>
 
           {/* Summer Section */}
-          <div className="absolute top-[350px] left-[1400px] flex items-start">
+          <div className="absolute top-[350px] left-[1100px] flex items-start">
             <img src={line} alt="Vector" className="absolute w-[170px] -bottom-3 left-1" />
             <span className="text-4xl font-bold text-black">Summer</span>
           </div>
-          <span className="absolute top-[450px] left-[1250px] text-lg text-black w-[500px] text-center">
+          <span className="absolute top-[450px] left-[900px] text-lg text-black w-[500px] text-center">
             Summer individuals have cool, soft, and muted colouring. They typically look best in cool-toned colours that have a soft, subdued quality. This season is characterized by a gentle, cool, and soft appearance.
           </span>
 
 
           {/* Autumn Section */}
-          <div className="absolute top-[650px] left-[350px] flex items-start">
+          <div className="absolute top-[650px] left-[200px] flex items-start">
             <img src={line} alt="Vector" className="absolute w-[170px] -bottom-3 left-1" />
             <span className="text-4xl font-bold text-black">Autumn</span>
           </div>
-          <span className="absolute top-[750px] left-[150px] text-lg text-black w-[500px] text-center">
+          <span className="absolute top-[750px] left-[50px] text-lg text-black w-[500px] text-center">
             Autumn types have warm, rich, and earthy tones in their overall colouring. Their skin, hair, and eyes are often characterized by warmth and depth, making them look stunning in deep, golden, and earthy shades.
           </span>
 
           {/* Winter Section */}
-          <div className="absolute top-[650px] left-[1400px] flex items-start">
+          <div className="absolute top-[650px] left-[1100px] flex items-start">
             <img src={line} alt="Vector" className="absolute w-[170px] -bottom-3 left-1" />
             <span className="text-4xl font-bold text-black">Winter</span>
           </div>
-          <span className="absolute top-[750px] left-[1250px] text-lg text-black w-[500px] text-center">
+          <span className="absolute top-[750px] left-[900px] text-lg text-black w-[500px] text-center">
             Winter types have cool, vivid, and high-contrast features, often looking striking in bold, cool colours. The Winter palette is made up of intense and clear shades that complement their naturally sharp, cool-toned appearance.
           </span>
 
           {/* Colour Wheel */}
-          <img src={Colourwheel} alt="colourwheel" className="absolute top-[400px] left-[700px] w-[500px] h-[500px]" />
+          <img src={Colourwheel} alt="colourwheel" className="absolute top-[450px] left-[550px] w-[350px] h-[350px]" />
 
         </div>
       </div>
